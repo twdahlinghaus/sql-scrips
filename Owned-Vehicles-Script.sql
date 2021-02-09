5dbcf3a4a0703ee3c2fc9f7eeff995be8e6d503f
Thomas Dahlinghaus <tdahlinghaus@maxtrain.com>
2021-02-08 23:00:19 -0500

    Initial commit for Owned Vehicles info

---
+++ Owned-Vehicles-Script.sql
@@ -0,0 +1,43 @@
+use OwnedVehicles;
+
+INSERT vehicles
+(Year, Make, Model, Trim, YearPurchased, StatePurchasedIn)
+VALUES
+('2019', 'RAM', '2500', 'Tradesman 4X4 Offroad', '2020', 'OH'),
+('1999', 'Chevrolet', 'Corvette', 'Z-51', '2019', 'OH'),
+('1996', 'Saturn', 'SL1', '4DR', '2020', 'OH'),
+('2018', 'RAM', '1500', 'Classic 4X4', '2018', 'WI'),
+('2003', 'Dodge', 'RAM 1500', '2X4', '2017', 'WI'),
+('2016', 'Dodge', 'Charger', 'R/T', '2017', 'IL'),
+('2014', 'KIA', 'SORENTO', 'EX', '2016', 'VA'),
+('2014', 'Dodge', 'Grand Caravan', 'SXT', '2014', 'VA'),
+('2012', 'KIA', 'Optima', 'EX', '2012', 'VA'),
+('2011', 'KIA', 'SORENTO', 'LX', '2012', 'VA'),
+('1998', 'Chevrolet', 'S-10', '4X4', '2015', 'VA'),
+('2010', 'GMC', 'Sierra', '4X4', '2011', 'MD'),
+('2010', 'VW', 'Jetta', 'GLI', '2013', 'VA'),
+('2010', 'Pontiac', 'GTO', '5.7L', '2009', 'VA'),
+('2006', 'Chevrolet', 'Cavalier', 'LS', '2010', 'MD'),
+('2006', 'Chrysler', 'Town & Country', 'LX', '2007', 'OH'),
+('2004', 'Ford', 'Mustang', 'GT', '2005', 'IL'),
+('2002', 'Nissan', 'Xterra', 'XE', '2003', 'IL'),
+('2000', 'Chrysler', '300M', 'Touring', '2001', 'IL'),
+('2003', 'Pontiac', 'Aztek', 'AWD', '2003', 'IL'),
+('1976', 'Triumph', 'TR6', 'Convertible', '2005', 'IL'),
+('1996', 'Nissan', '300ZX', 'Base', '2004', 'IL'),
+('2000', 'Ford', 'Explorer', 'XLT 4X4', '2001', 'WA'),
+('1999', 'Ford', 'Contour', 'SE', '2000', 'WA'),
+('1996', 'Pontiac', 'Trans AM', 'GT', '1998', 'HI'),
+('1976', 'Porsche', '914', '2.0', '1999', 'HI'),
+('1996', 'Chevrolet', 'Cavalier', 'Coupe', '1998', 'HI'),
+('1990', 'Mercury', 'Cougar', 'XR7 Supercharged', '1997', 'HI'),
+('1996', 'Dodge', 'Stratus', 'ES', '1998', 'HI'),
+('1990', 'VW', 'Jetta', 'GL', '1997', 'FL'),
+('1986', 'Pontiac', 'Fiero', 'GT', '1995', 'OH'),
+('19XX', 'Ford', 'Thunderbird', 'Base', '1995', 'OH'),
+('19XX', 'Buick', 'Regal', 'Sedan', '1994', 'OH'),
+('1986', 'Ford', 'Escort', 'Base', '1994', 'OH'),
+('1965', 'Plymouth', 'Fury', 'III', '1994', 'OH');
+
+select * from vehicles;
+Select * from vehicles;
